3a25aa4f0bd7eafdb078f985c16fff73422bd73c
Hamza Sajid <h.sajid@hacken.io>
2025-01-15 12:34:31 +0500

    updated issues in github oauth flow

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::env;
+use std::{sync,env};
 use std::sync::{Arc, Mutex};
 use tokio;
 
@@ -14,18 +14,16 @@ struct OAuthCallback {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let client_id = env::var("clientID").expect("Missing clientID in .env");
-    let client_secret = env::var("clientSecret").expect("Missing clientSecret in .env");
-    let redirect_uri = env::var("redirectURI").expect("Missing redirectURI in .env");
-
-    let access_token = Arc::new(Mutex::new(String::new()));
+    let shared_state = AppState {
+        client_id: env::var("clientID").expect("Missing clientID in .env"),
+        client_secret: env::var("clientSecret").expect("Missing clientSecret in .env"),
+        redirect_uri: env::var("redirectURI").expect("Missing redirectURI in .env"),
+        access_token: sync::Arc::new(tokio::sync::Mutex::new(String::new())),
+    };
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(client_id.clone()))
-            .app_data(web::Data::new(client_secret.clone()))
-            .app_data(web::Data::new(redirect_uri.clone()))
-            .app_data(web::Data::new(access_token.clone()))
+            .app_data(web::Data::new(shared_state.clone()))
             .route("/login/oauth", web::get().to(login_oauth))
             .route("/oauth-callback", web::get().to(oauth_callback))
             .route("/repos/{owner}/{repo}/tree-content", web::get().to(fetch_repo_tree))
@@ -35,42 +33,50 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-async fn login_oauth(client_id: web::Data<String>, redirect_uri: web::Data<String>) -> impl Responder {
+// Shared application state
+#[derive(Clone)]
+struct AppState {
+    client_id: String,
+    client_secret: String,
+    redirect_uri: String,
+    access_token: std::sync::Arc<tokio::sync::Mutex<String>>,
+}
+
+async fn login_oauth(
+    data: web::Data<AppState>
+) -> impl Responder {
     let auth_url = format!(
         "https://github.com/login/oauth/authorize?client_id={}&redirect_uri={}&scope=repo",
-        client_id.get_ref(),
-        redirect_uri.get_ref()
+        data.client_id, data.redirect_uri
     );
-    HttpResponse::Found().header("Location", auth_url).finish()
+    HttpResponse::Found().append_header(("Location", auth_url)).finish()
 }
 
 async fn oauth_callback(
     query: web::Query<OAuthCallback>,
-    client_id: web::Data<String>,
-    client_secret: web::Data<String>,
-    redirect_uri: web::Data<String>,
-    access_token: web::Data<Arc<Mutex<String>>>,
+    data: web::Data<AppState>,
 ) -> impl Responder {
     let token_url = "https://github.com/login/oauth/access_token";
     let params = [
-        ("client_id", client_id.get_ref()),
-        ("client_secret", client_secret.get_ref()),
+        ("client_id", &data.client_id),
+        ("client_secret", &data.client_secret),
         ("code", &query.code),
-        ("redirect_uri", redirect_uri.get_ref()),
+        ("redirect_uri", &data.redirect_uri),
     ];
 
     let client = reqwest::Client::new();
-    match client
+    let response = client
         .post(token_url)
         .header("Accept", "application/json")
         .form(&params)
         .send()
-        .await
-    {
+        .await;
+    match response {
         Ok(resp) => match resp.json::<Value>().await {
             Ok(json) => {
                 if let Some(token) = json.get("access_token").and_then(|v| v.as_str()) {
-                    *access_token.lock().unwrap() = token.to_string();
+                    let mut access_token = data.access_token.lock().await;
+                    *access_token = token.to_string();
                     HttpResponse::Ok().json("Authentication successful!")
                 } else {
                     HttpResponse::InternalServerError().json("Failed to extract access token")
@@ -78,7 +84,10 @@ async fn oauth_callback(
             }
             Err(_) => HttpResponse::InternalServerError().json("Failed to parse JSON response"),
         },
-        Err(_) => HttpResponse::InternalServerError().json("Failed to get access token"),
+        Err(err) => {
+            eprintln!("Error while requesting access token: {:?}", err);
+            HttpResponse::InternalServerError().json("Failed to get access token")
+        }
     }
 }
 
